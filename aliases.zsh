9ee7e7efd05392c0639cffc6b6c3c27403655ed0
Marcel Kalveram <marcel.kalveram@googlemail.com>
2019-10-07 18:02:35 +0300

    add react-native ios simulator commands to aliases

--- aliases.zsh
+++ aliases.zsh
@@ -29,9 +29,21 @@ alias gl="git log --oneline --decorate --color"
 alias gnuke="git clean -df && git reset --hard"
 
 # Alias for React Native packager
-alias rnios='react-native run-ios --simulator "iPhone 8"'
+alias rnios='react-native run-ios'
 alias rniosreset="kill \$(lsof -t -i:8081); rm -rf ios/build/; react-native run-ios"
 alias rnan='react-native run-android'
+
+# React Native simulators
+alias rnios-6='react-native run-ios --simulator "iPhone 6"'
+alias rnios-6p='react-native run-ios --simulator "iPhone 6 Plus"'
+alias rnios-7='react-native run-ios --simulator "iPhone 7"'
+alias rnios-7p='react-native run-ios --simulator "iPhone 7 Plus"'
+alias rnios-8='react-native run-ios --simulator "iPhone 8"'
+alias rnios-8p='react-native run-ios --simulator "iPhone 8 Plus"'
+alias rnios-x='react-native run-ios --simulator "iPhone X"'
+alias rnios-xs='react-native run-ios --simulator "iPhone XS"'
+alias rnios-xsm='react-native run-ios --simulator "iPhone XS Max"'
+alias rnios-xr='react-native run-ios --simulator "iPhone XR"'
 
 # Xcode
 alias xcs101='sudo xcode-select -s /Applications/Xcode_10.1.app/'
@@ -46,3 +58,16 @@ function take {
 }
 
 function cdl { cd $1; ls;}
+
+# iOS - find architectures in framework file (e.g. 'architectures XYZ.framework/XYZ')
+function architectures() {
+  if $(lipo -info $1 | grep -q 'not a fat file'); then
+    lipo -info $1 | awk -F ': ' '/Non-fat file/ { print $3 }'
+  else
+    lipo -detailed_info $1 | awk '/^architecture / { print $2 }'
+  fi
+}
+
+if [[ $(basename $0) == 'architectures' ]]; then
+  architectures $1
+fi
